85ee1f334bb2232c3a303c781d8fd396168f3de8
LetsBash.de <90566696+LetsBashDE@users.noreply.github.com>
2023-03-03 19:59:55 +0100

    Update engine.ps1

--- engine.ps1
+++ engine.ps1
@@ -1,8 +1,8 @@
 # Autor: LetsBash.de / SirBash.com
 
 function retriveLatestSavegame {
-    $lastestpath = $false;
-    $lastesttime = $false;
+    $lastestpath = $false
+    $lastesttime = $false
     $savepath = ($ENV:LOCALAPPDATA + "low\Endnight\SonsOfTheForest\Saves\")
     $steamidfolders = Get-ChildItem -path $savepath
     foreach ($steamidfolder in $steamidfolders) {
@@ -22,7 +22,7 @@ function retriveLatestSavegame {
                     $lastestpath = $savegamefolder.fullname
                 }
     
-                if ($lastesttime -gt $lastesttime) {
+                if ($modifiedtime -gt $lastesttime) {
                     $lastesttime = $modifiedtime
                     $lastestpath = $savegamefolder.fullname
                 }
